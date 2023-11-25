8cb82618e238a2306cf031ab0b1551a304ca1b38
Ripul Handoo <ripulhandoo1234@gmail.com>
2023-11-25 19:20:04 +0530

    Test file to validate ci/cd pipeline

---
+++ test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func test() {
+	// this is just a test file to check if github action is working correctly or not
+	log.Println("This is a test file")
+	fmt.Println("This is a test file")
+	err := http.ListenAndServe("8080", nil)
+	if err != nil {
+		log.Fatal("Error in starting the server")
+	}
+}
