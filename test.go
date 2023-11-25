b07c77d88bcc89b17a1af725ae075ecca8d4f356
Ripul Handoo <107461226+RipulHandoo@users.noreply.github.com>
2023-11-25 19:52:50 +0530

    Update test.go

--- test.go
+++ test.go
@@ -10,7 +10,7 @@ func test() {
 	// this is just a test file to check if github action is working correctly or not
 	log.Println("This is a test file")
 	fmt.Println("This is a test file")
-	err := http.ListenAndServe("8080", nil)
+	err := http.ListenAndServe("880", nil)
 	if err != nil {
 		log.Fatal("Error in starting the server")
 	}
